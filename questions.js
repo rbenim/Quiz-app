3de9855c08df5646d3bfe47b49bddb23a03ef199
exampregh <98620311+examprepgh@users.noreply.github.com>
2022-03-19 15:29:02 +0000

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,378 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "As part of a science lesson, pupils were asked to stretch a spring to extend its length by 50 per cent.The original length of the spring was 30 centimetres. What should be the length of the extended spring?",
+    answer: "45 cm",
+    options: [
+      "15 cm",
+      "50 cm",
+      "45 cm",
+      "35 cm"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The number of rooms at Hotel G is 10 less than twice the number of rooms at Hotel H. If the total number of rooms at both Hotels is 425, what is the number of rooms at Hotel G?.       Hint: let the number of rooms in H = y, therefore 2y - 10 + y = 425 ",
+    answer: "280",
+    options: [
+      "140",
+      "180",
+      "240",
+      "280"
+    ]
+  },
+    {
+    numb: 3,
+    question: "In an election between 2 candidates, one got 55% of the total valid votes, 20% votes were invalid. If the total number of votes was 7500, what was the number of valid votes that the other candidate got?",
+    answer: "2700",
+    options: [
+      "2800",
+      "2700",
+      "3300",
+      "2500"
+    ]
+  },
+    {
+    numb: 4,
+    question: "A company has 200 employees. Thirty nine percent of them work in the machine room and 32% are engaged in distribution of goods. How many of the employees are engaged in other duties apart from these two? ?",
+    answer: "58",
+    options: [
+      "58",
+      "45",
+      "56",
+      "71"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Six hundred copies of a book have to be packed into boxes. Each box can hold 15 of the books. Which one of the following operations could be used to find the number of boxes needed?",
+    answer: "Divide 600 by 15",
+    options: [
+      "Add 15 to 600",
+      "Multiply 600 by 15",
+      "Subtract 15 from 600",
+      "Divide 600 by 15"
+    ]
+  },
+
+
+  
+
+     {
+     numb: 6,
+     question: "What is the average (arithmetic mean) of all the multiples of ten from 10 to 190 inclusive?",
+     answer: "100",
+     options: [
+       "90",
+       "100",
+       "105",
+       "90"
+     ]
+   },
+
+   {
+    numb: 7,
+    question: ". In a class of 24 pupils, 4 need additional support. What fraction of the class in lowest term needs additional support?",
+    answer: "1/6",
+    options: [
+      "1/6",
+      "1/8",
+      "1/4",
+      "1/2"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "There are 3 apples and you take away 2. How many do you have?",
+    answer: "2",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: " If an object travels at five feet per second, how many feet does it travel in one hour",
+    answer: "18000",
+    options: [
+      "30",
+      "300",
+      "1800",
+      "18000"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: " If an object travels at five feet per second, how many feet does it travel in one hour",
+    answer: "18000",
+    options: [
+      "30",
+      "300",
+      "1800",
+      "18000"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "Yaw, Kofi and Ama shared the profit they earned in a month from a joint business in the ratio 2:4:3 respectively. If Ama and Kofi together got a total of GH 42.00. Find the profit earned in the month.",
+    answer: "GH 54.00",
+    options: [
+      "GH 50.40",
+      "GH 62.00",
+      "GH 54.00",
+      "GH 68.00"
+    ]
+  },
+
+  {
+    numb: 12,
+    question: "A woman bought 210 oranges for 65 Ghanaian Cedis. She sold all of them at 3 for 2 cedis. How much profit did she make?",
+    answer: "75 cedis",
+    options: [
+      "35 cedis",
+      "45 cedis",
+      "65 cedis",
+      "75 cedis"
+    ]
+  },
+
+  {
+    numb: 13,
+    question: "Simplify - 27 + 18 - (10 - 14) - (-2).",
+    answer: "-3",
+    options: [
+      "-3",
+      "-7",
+      "-11",
+      "-35"
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "Which of the following statements is TRUE about natural numbers?",
+    answer: "Composite numbers have more than two divisors",
+    options: [
+      "All prime numbers are odd",
+      "Composite numbers have more than two divisors",
+      "The sum of any two natural numbers areodd",
+      "Even numbers are not divisible by 2"
+    ]
+  },
+
+  {
+    numb: 15,
+    question: ". Mansah obtained 150 marks out of 240 marks in a Mathematics test. What was her percentage score?. Hint: 150/240 x 100%",
+    answer: "62.50%",
+    options: [
+      "36.50%",
+      "33.3%",
+      "41.67%",
+      "62.50%"
+    ]
+  },
+
+  {
+    numb: 16,
+    question: "What is the next tern in the pattern ? 4 4 = 0, 4 - 3 = 1, 4 - 2 = 2, 4 - 1 = 3, 4 - 0  = 4, __ - __ = 5.",
+    answer: "4 - ( - 1) = 5",
+    options: [
+      "4 - ( - 1) = 5",
+      "4 - ( - 2) = 6",
+      "4 - 5 = -1",
+      "4 - 1 = 3"
+    ]
+  },
+
+  {
+    numb: 16,
+    question: " It takes 15 men an average of 48 days to weed a plot of land. How many men can weed the same plot of land in 16 days, if they work at the same rate?.  Hint: you can use ratio in solving this, 15x48 /16",
+    answer: "45",
+    options: [
+      "32",
+      "5",
+      "45",
+      "18"
+    ]
+  },
+
+  {
+    numb: 17,
+    question: "Some pages in a book on a bookshelf are missing so that page 21 is now followed immediately by page 43. How many pages are missing?",
+    answer: "22",
+    options: [
+      "43",
+      "20",
+      "21",
+      "22"
+    ]
+  },
+
+  {
+    numb: 18,
+    question: "Eleven more than 2 times Jona's age is equal to 59. How old is Jona?. Hint: let Joana's age = y. 11 + 2y = 59",
+    answer: "24 years",
+    options: [
+      "23 yaers",
+      "24 years",
+      "67 years",
+      "35 years"
+    ]
+  },
+
+  {
+    numb: 19,
+    question: "Alexander Addae bought three items priced at GH 65.50, GH 36.45 and GH 15.78. He paid with three GH 50.00 notes. How much change did he received?",
+    answer: "GH 32.27",
+    options: [
+      "GH 32.73",
+      "GH 33.73",
+      "GH 32.27",
+      "GH 32.73"
+    ]
+  },
+
+  {
+    numb: 20,
+    question: "Dartey recorded a mean score of 24 on five tests. What should she score on the sixth test to bring her mean score up to 26?",
+    answer: "-3",
+    options: [
+      "30",
+      "32",
+      "36",
+      "40"
+    ]
+  },
+
+  {
+    numb: 21,
+    question: ". A school wants to fence its rectangular playing field measuring 118m by 92m with wire mesh. How many meters of wire mesh will be needed to complete the fencing?. Hint: lenght= 118 and width= 92. rectangle has 2 Lengh and 2 Width. Therefore 2l + 2w ",
+    answer: "420m",
+    options: [
+      "200m",
+      "400m",
+      "420m",
+      "440m"
+    ]
+  },
+
+  {
+  numb: 22,
+    question: "A PE teacher orders 30 new bibs costing £3.99 each. What is the total cost of these bibs?",
+    answer: "£ 119.70",
+    options: [
+      "£ 33.99",
+      "£ 120.00",
+      "£ 119.70",
+      "£ 182.10"
+    ]
+  },
+
+  {
+    numb: 23,
+      question: "The population of two communities are 1.8705 x 106 and 9.00846 x 105. Calculate the total population of the two communities",
+      answer: "2,771,365",
+      options: [
+        "277,145",
+        "2,771,365",
+        "2,972,000",
+        "10,880,000"
+      ]
+    },
+
+    {
+    numb: 24,
+    question: "If the population of a village increases by 97, it will reach at least 4,050 people. Which one of the following inequalities represents the statement correctly? ",
+    answer: "P + 97 ≤  4,050",
+    options: [
+      "P + 97 ≤  050",
+      "P + 97 < 4,050",
+      "P + 97 ≤  4,050",
+      "P + 97 > 4,050"
+    ]
+  },
+
+  {
+    numb: 25,
+    question: "A school trip was planned at a total cost of £100 per pupil. The accommodation cost three fifths of the total. What was the cost of the accommodation per pupil?. Hint: Accomodation = 3/5 x 100",
+    answer: "£60",
+    options: [
+      "£50",
+      "£70",
+      "£60",
+      "£66"
+    ]
+  },
+
+  {
+    numb: 26,
+    question: "Which two temperatures are closest to each other?",
+    answer: "-6 degrees Celsius and -3 degrees Celsius",
+    options: [
+      "-6 degrees Celsius and -3 degrees Celsius",
+      "–2 degrees Celsius and 3 degrees Celsius",
+      "2 degrees Celsius and 6 degrees Celsius",
+      "–3 degrees Celsius and 3 degrees Celsius"
+    ]
+  },
+
+   {
+    numb: 27,
+    question: "Eight out of 25 pupils scored full marks in a test. What percentage of pupils scored full marks?",
+    answer: "32%",
+    options: [
+      "18%",
+      "5%",
+      "32%",
+      "35%"
+    ]
+  },
+
+   {
+    numb: 28,
+    question: "A man gave 20% of his salary to the mother and 35% to the father. What percentage of the money is left?",
+    answer: "45%",
+    options: [
+      "47",
+      "45%",
+      "43%",
+      "63%"
+    ]
+  },
+  
+   {
+    numb: 29,
+    question: "There are x boys and y girls in a classroom. Each pupil had 2 pens. Which of these expressions represents the total number of pens carried to the classroom?",
+    answer: "2(x + y)",
+    options: [
+      "2(x + y)",
+      "2 + (x + y)",
+      "2xy",
+      "2x + y"
+    ]
+  },
+
+   {
+    numb: 30,
+    question: "A student bought a story book from a store. He gave the cashier GH¢15.00 and got back GH¢8.00 in change. The student saw that he had gotten too much change and he gave GH¢2.00 back. What was the cost of the story book?",
+    answer: "GH¢9.00",
+    options: [
+      "GH¢9.50",
+      "GH¢8.70",
+      "GH¢9.00",
+      "GH¢7.80"
+    ]
+  },
+
+   
+  
+
+
+];
